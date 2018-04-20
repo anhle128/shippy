63bb37f6a12c85f16f31b549103f496005a50a23
anhle <anhle@ttab.me>
2018-04-20 16:57:43 +0700

    save code

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -2,43 +2,71 @@
 package main
 
 import (
-
-	// Import the generated protobuf code
+	"context"
 	"fmt"
-	"log"
-
-	"os"
 
 	pb "github.com/anhle128/shippy/consignment-service/proto/consignment"
-	vesselProto "github.com/anhle128/shippy/vessel-service/proto/vessel"
-	"github.com/micro/go-micro"
+	micro "github.com/micro/go-micro"
 )
 
-const (
-	defaultHost = "localhost:27017"
-)
+type IRepository interface {
+	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
+}
+
+// Repository - Dummy repository, this simulates the use of a datastore
+// of some kind. We'll replace this with a real implementation later on.
+type Repository struct {
+	consignments []*pb.Consignment
+}
+
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	updated := append(repo.consignments, consignment)
+	repo.consignments = updated
+	return consignment, nil
+}
+
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
+}
+
+// Service should implement all of the methods to satisfy the service
+// we defined in our protobuf definition. You can check the interface
+// in the generated code itself for the exact method signatures etc
+// to give you a better idea.
+type service struct {
+	repo IRepository
+}
+
+// CreateConsignment - we created just one method on our service,
+// which is a create method, which takes a context and a request as an
+// argument, these are handled by the gRPC server.
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+
+	// Save our consignment
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return err
+	}
+
+	// Return matching the `Response` message we created in our
+	// protobuf definition.
+	res.Created = true
+	res.Consignment = consignment
+	return nil
+}
+
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
+	consignments := s.repo.GetAll()
+	res.Consignments = consignments
+	return nil
+}
 
 func main() {
 
-	// Database host from the environment variables
-	host := os.Getenv("DB_HOST")
+	// cmd.Init()
 
-	if host == "" {
-		host = defaultHost
-	}
-
-	session, err := CreateSession(host)
-
-	// Mgo creates a 'master' session, we need to end that session
-	// before the main function closes.
-	defer session.Close()
-
-	if err != nil {
-
-		// We're wrapping the error returned from our CreateSession
-		// here to add some context to the error.
-		log.Panicf("Could not connect to datastore with host %s - %v", host, err)
-	}
+	repo := &Repository{}
 
 	// Create a new service. Optionally include some options here.
 	srv := micro.NewService(
@@ -48,13 +76,11 @@ func main() {
 		micro.Version("latest"),
 	)
 
-	vesselClient := vesselProto.NewVesselServiceClient("go.micro.srv.vessel", srv.Client())
-
 	// Init will parse the command line flags.
 	srv.Init()
 
 	// Register handler
-	pb.RegisterShippingServiceHandler(srv.Server(), &handler{vesselClient, session})
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
 
 	// Run the server
 	if err := srv.Run(); err != nil {
